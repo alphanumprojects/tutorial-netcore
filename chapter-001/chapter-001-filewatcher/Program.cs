14c31c23e67439c66bcacfe9e54c54e766d6cfca
alpha-projects <mm.projectlab@gmail.com>
2024-01-16 22:26:57 +0700

    file watcher

---
+++ chapter-001/chapter-001-filewatcher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string folderPath = @"D:\Works\internal\alpha-projects\temp-file"; // Ganti dengan path folder Anda
+
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        watcher.Path = folderPath;
+        watcher.NotifyFilter = NotifyFilters.FileName;
+
+        watcher.Created += OnCreated;
+
+        watcher.EnableRaisingEvents = true;
+
+        Console.WriteLine("Memantau folder. Tekan 'q' untuk keluar.");
+        while (Console.ReadKey().Key != ConsoleKey.Q) { }
+
+        watcher.EnableRaisingEvents = false;
+    }
+
+    private static void OnCreated(object sender, FileSystemEventArgs e)
+    {
+        try
+        {
+            Console.WriteLine($"File baru terdeteksi: {e.Name}");
+
+            // Lakukan pembacaan atau proses yang diperlukan pada file baru di sini
+            // Contoh: Baca isi file
+            string content = File.ReadAllText(e.FullPath);
+            Console.WriteLine($"Isi file {e.Name}:");
+            Console.WriteLine(content);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
